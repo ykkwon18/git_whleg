f12e10f5dc65b61b460c7cff0710507c4971459c
ykkwon18 <yk.kwon18@gmail.com>
2025-04-14 20:51:35 +0900

    walk_test algorithm optimization
    2족 보행 테스트 펌웨어의 코드 최적화. 벨로그 업로드 버전.

--- openRB150 firmware/walk_test.c
+++ openRB150 firmware/walk_test.c
@@ -23,21 +23,18 @@
 
 const float PROTOCOL_VERSION = 2.0;
 
-const uint8_t DXL_ID1 = 1;
-const uint8_t DXL_ID2 = 2;
-
 const int start = 1;
 const int stop = 0;
 
 Dynamixel2Arduino dxl(DXL_SERIAL, 57600);
 
-float theta_M = 0.0;      // 휠의 모터 각도
+float theta_H = 0.0;      // 휠의 모터 각도
 float theta_L = 0.0;      // 다리의 모터 각도
 float x = 0.0;            // 다리 x좌표 계산을 위한 전역변수
 float y = 0.0;            // 다리 y좌표 계산을 위한 전역변수
 
-float L1 = 0.035;         // 링크1 길이 (m)
-float L2 = 0.085; 	      // 링크2 길이 (m)
+float L1 = 35;         // 링크1 길이 (mm)
+float L2 = 85; 	      // 링크2 길이 (mm)
 
 float dt = 0.05;  // dt = 0.05초
 float t = 0.0;   // 시간 변수
@@ -47,7 +44,7 @@ float tstep = 2.0;  // 시간 스텝 길이 (2초)
 // 아두이노 작동 시작 세팅
 // 모든 모터 작동모드를 OP_POSITION으로 설정하고, 토크를 킨다
 // 모든 모터의 포지션을 2048(0도)로 설정
-// mode3 한 번 구동
+// mode2 한 번 구동
 void setup() {
 	USB_SERIAL.begin(57600);                            // 통신속도 설정
 	dxl.begin(57600);                                   // 다이나믹셀 통신 시작
@@ -64,20 +61,12 @@ void setup() {
 
 	Serial.println("✅ OpenRB-150 펌웨어 시작됨!");
 
-	/*
-	for(int i = 1; i <= 2; i++){
-		dxl.setGoalPosition(i, 2048);  // 모든 모터를 0도로 초기화
-		dxl.setGoalPosition(i+1, 2048);
-	}
-	delay(2000);
-	*/
-
 	Erection();  // 직립 자세로 설정
 	delay(5000); // 5초 대기
 
 	while() {
-		computeLegIK(2,3);   //2번(init 포물선) 모드, 1번 다리       || 왼다리 전진. 오른다리 후진.
-		computeLegIK(0,1);   //0번(직선) 모드, 3번 다리
+		computeLegIK(2,3);   //2번(init 포물선) 모드, 3번 다리       || 왼다리 전진. 오른다리 후진.
+		computeLegIK(3,1);   //3번(init line) 모드, 1번 다리
 		t += dt;
 		if(t > tstep){      //tstep까지 작동하면 초기화 및 break
 			t = 0.0;
@@ -115,61 +104,44 @@ void loop(){
 	}
 }
 
-
-/*
-// -------------------- 시리얼 명령 읽기 --------------------
-bool readSerialCommand() {
-	if (!USB_SERIAL.available()) return false;
-
-	String data = USB_SERIAL.readStringUntil('\n');
-	data.trim();
-
-    return (data == "start");
-}*/
-
-
-//LegMoving
+//Command position to motor
 void computeLegIK(int mode, int i) {
 
 	Parabola(mode);  // compute x,y of Leg 
-	InvKin();       // IK -> theta_M theta_L
+	InvKin();       // IK -> theta_H theta_L
 
 	if(i ==3 || i == 5){  // 왼쪽 다리일 경우, theta_M을 반전시킴
-		theta_M = -theta_M;
+		theta_H = -theta_H;
 		theta_L = -theta_L;
 	}
 
-	// 각도(rad) → 다이나믹셀 0~4095로 변환
-	int pos_M = map(theta_M * 180 / PI, -150, 150, 0, 4095);
-	int pos_L = map(theta_L * 180 / PI, -150, 150, 0, 4095);
+	float pos_H = 2048 + (theta_H * 2048 / PI);  // theta_H(rad) -> pos_H(0~4095) || 0도는 2048
+	float pos_L = 2048 + (theta_L * 2048 / PI);  // theta_L(rad) -> pos_L(0~4095) || 0도는 2048
 
 	// 값 제한 (실제 모터 가동 범위 초과 방지)
-	pos_M = constrain(pos_M, 0, 4095);
+	pos_H = constrain(pos_H, 0, 4095);
 	pos_L = constrain(pos_L, 0, 4095);
 
-	dxl.setGoalPosition(i, pos_M);
+	dxl.setGoalPosition(i, pos_H);
 	dxl.setGoalPosition(i+1, pos_L);
 }
 
 //comtpute IK
 void InvKin() {
-    float D = sqrt(x * x + y * y);
-    float cos_theta_L = (x * x + y * y - L1 * L1 - L2 * L2) / (2 * L1 * L2);
-    cos_theta_L = constrain(cos_theta_L, -1.0, 1.0);
+	float k1, k2, cos2, sin2;  // k1, k2는 링크 길이와 관련된 변수
+	
+    cos2 = (x*x + y*y - L1*L1 - L2*L2) / (2*L1*L2);  // cos(theta_L) 계산
+	if(cos2*cos2>1){
+		println("cos(theta_L) > 1.0");  // cos(theta_L) 값이 1보다 크면 오류 발생
+		return;
+	}
+	sin2 = sqrt(1 - cos2*cos2);  // sin(theta_L) 계산 || 무릎을 앞으로 굽힐 것이므로, sin(theta_L)은 양수로 설정
 
-    // theta_L는 항상 양수 (Elbow-up)
-    theta_L = acos(cos_theta_L);  // 0 ~ PI (0° ~ 180°)
+	k1 = L1 + L2*cos2;  // k1, k2 계산
+	k2 = L2*sin2;  // k2는 항상 양수로 설정
 
-    // theta_M 계산
-    float alpha = atan2(y, x);
-    float cos_beta = (x * x + y * y - L2 * L2 + L1 * L1) / (2 * L1 * D);
-    cos_beta = constrain(cos_beta, -1.0, 1.0);
-    float beta = acos(cos_beta);
-    theta_M = alpha - beta - PI/2;  // 로봇의 head 방향이 x축을 향하도록 설정했는데, 모터의 세타값이 0도일 때, y방향을 향함. 따라서 -PI/2(90도)를 해줌
-
-    // 디버깅 출력 (선택적)
-    //Serial.print("theta_M: "); Serial.print(theta_M * 180 / PI);
-    //Serial.print(", theta_L: "); Serial.println(theta_L * 180 / PI);
+	theta_L = atan2(cos2, sin2);            // theta_L 계산(rad)
+	theta_H = atan2(y, x) - atan2(k2, k1);  // theta_H 계산(rad)
 }
 
 //현재 t와 mode에 대해 x,y좌표를 계산하는 함수
@@ -178,19 +150,23 @@ void Parabola(int mode) {
 	switch (mode)
 	{
 		case 0:  // line
-			x = -0.06*t + 0.06;
-			y = 0.08;
+			y = -60.0 + 60.0*t;  // y좌표가 -60 ~ 60
+			x = 80.0;
 			break;
 		
 		case 1:  // parabola
-			x = 0.12*t - 0.06;
-			y = 50/9*x*x + 0.06;
+			y = 60.0 - 60.0*t;   // y 좌표가 60 ~ -60
+			x = 1.0/120.0*y*y + 50.0;
+			break;
+		
+		case 2: // init parabola
+			y = -30.0*t;   // y 좌표가 0 ~ -60
+			x = 1.0/30.0*(y+30.0)*(y+30.0) + 50.0;
 			break;
 
-		case 2:  // parabola init
-			x = 0.06*t;
-			y = 200/9*(x*x-0.06*x+0.03*0.03) + 0.06;
-			break;
+		case 3: // init line
+			y = 30.0*t;   // y 좌표가 0 ~ 60
+			x = 80.0;
 
 		default:
 			Serial.println("parabola error");
@@ -199,8 +175,9 @@ void Parabola(int mode) {
 	
 }
 
-void Erection(){     // 직립 자세로 설정 함수. 내장 딜레이 없음
-	t = tstep;      //0번 모드 종단 위치로 발을 이동시키기 위해 t를 tstep로 설정
+// 다리 위치를 직립 초기화. 내장 딜레이는 없음.
+void Erection(){
+	t = tstep/2;      //0번 모드 중간 위치로 발을 이동시키기 위해 t를 tstep/2로 설정
 	computeLegIK(0,1);   //1번 다리 직립 위치
 	computeLegIK(0,3);   //3번 다리 직립 위치
 	t = 0.0;  // t 초기화
